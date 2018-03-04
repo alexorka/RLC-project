74a73432bbad0807fbc2cbf8e18d9f7407568414
alexorka <36878708+alexorka@users.noreply.github.com>
2018-03-04 16:47:49 +0300

    Добавьте файлы проекта.

---
+++ LRC-MVC5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LRC_MVC5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
