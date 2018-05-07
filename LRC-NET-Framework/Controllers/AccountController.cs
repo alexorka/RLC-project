f4b358d46384e84d5e2777322605d40093f02d45
alexorka <36878708+alexorka@users.noreply.github.com>
2018-05-07 23:59:45 +0300

    Import Full Time

--- LRC-NET-Framework/Controllers/AccountController.cs
+++ LRC-NET-Framework/Controllers/AccountController.cs
@@ -12,6 +12,12 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using LRC_NET_Framework.Models;
 using System.Collections.Generic;
+using ExcelImport.Models;
+using LinqToExcel;
+using System.Data.OleDb;
+using System.IO;
+using System.Data;
+using System.Data.Entity.Validation;
 
 //using System.Web.Http;
 
@@ -223,9 +229,9 @@ namespace LRC_NET_Framework.Controllers
         }
 
         //
-        // GET: /Account/Register
+        // GET: /Account/AdminTasks
         [Authorize(Roles = "admin")]
-        public ActionResult ConfirmRegistration()
+        public ActionResult AdminTasks(string xlsSelectResult)
         {
             var _users = db.AspNetUsers.ToList();
             List<SelectListItem> _UsersRoles = new List<SelectListItem>();
@@ -238,6 +244,7 @@ namespace LRC_NET_Framework.Controllers
                     _UsersRoles.Add(new SelectListItem() { Text = _user.Email, Value = currentRoles });
             }
             ViewBag.UsersAndRoles = _UsersRoles;
+            ViewBag.ResultMessage = xlsSelectResult ?? String.Empty;
 
             return View();
         }
@@ -259,7 +266,7 @@ namespace LRC_NET_Framework.Controllers
                 //await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
             }
             AddErrors(result);
-            return RedirectToAction("ConfirmRegistration", "Account");
+            return RedirectToAction("AdminTasks", "Account");
         }
 
         //
@@ -282,7 +289,7 @@ namespace LRC_NET_Framework.Controllers
                 //await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
             }
             AddErrors(result);
-            return RedirectToAction("ConfirmRegistration", "Account");
+            return RedirectToAction("AdminTasks", "Account");
         }
 
 
@@ -567,6 +574,175 @@ namespace LRC_NET_Framework.Controllers
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
             return RedirectToAction("Index", "Home");
+        }
+
+        /// <summary>  
+        /// This function is used to download excel format.  
+        /// </summary>  
+        /// <param name="Path"></param>  
+        /// <returns>file</returns>  
+        public FileResult DownloadExcel()
+        {
+            string path = "/AdminTasks/CBU.xlsx";
+            return File(path, "application/vnd.ms-excel", "CBU.xlsx");
+        }
+
+        // Extract Last, First, Middle Names from FullName 
+        private string SplitFullName(string _fullName, out string _lastName, out string _firstName, out string _middleName)
+        {
+            string result = "Success";
+            _lastName = String.Empty;
+            _firstName = String.Empty;
+            _middleName = String.Empty;
+            var namesComma = _fullName.Split(',');
+            if (namesComma.Length == 0)
+                result = "Empty 'Name' field";
+            else if (namesComma.Length == 1)
+                result = "Comma is absent in 'Name' field";
+            else if (namesComma.Length == 2)
+            {
+                _lastName = namesComma[0];
+                var namesSpace = namesComma[1].Split(' ');
+                if (namesSpace.Length == 1)
+                    _firstName = namesSpace[0];
+                else if (namesSpace.Length == 2)
+                {
+                    _firstName = namesSpace[0];
+                    _middleName = namesSpace[1];
+                }
+            }
+            return result;
+        }
+
+        //// GET: AdminTasks
+        //public ActionResult UploadExcel()
+        //{
+        //    ViewBag.ResultMassage = String.Empty;
+        //    return View();
+        //}
+
+        // POST: AdminTasks
+        [HttpPost]
+        public ActionResult UploadExcel(HttpPostedFileBase FileUpload)
+        {
+            string message = String.Empty;
+            List<string> data = new List<string>();
+            if (FileUpload != null)
+            {
+                // tdata.ExecuteCommand("truncate table OtherCompanyAssets");  
+                if (FileUpload.ContentType == "application/vnd.ms-excel" || FileUpload.ContentType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                {
+                    string filename = FileUpload.FileName;
+                    string filepath = "~/ImportExcel/1/";
+                    if (!Directory.Exists(Server.MapPath(filepath)))
+                        Directory.CreateDirectory(Server.MapPath(filepath));
+                    string targetpath = Server.MapPath(filepath);
+                    FileUpload.SaveAs(targetpath + filename);
+                    string pathToExcelFile = targetpath + filename;
+                    var connectionString = "";
+                    if (filename.EndsWith(".xls"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", pathToExcelFile);
+                    }
+                    else if (filename.EndsWith(".xlsx"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1\";", pathToExcelFile);
+                    }
+
+                    var adapter = new OleDbDataAdapter("SELECT * FROM [Full time$]", connectionString);
+                    var ds = new DataSet();
+
+                    adapter.Fill(ds, "ExcelTable");
+
+                    DataTable dtable = ds.Tables["ExcelTable"];
+
+                    string sheetName = "Full time";
+
+                    var excelFile = new ExcelQueryFactory(pathToExcelFile);
+                    var members = from a in excelFile.Worksheet<ExcelMembers>(sheetName) select a;
+
+                    foreach (var a in members)
+                    {
+                        try
+                        {
+                            if (a.Name != "" && a.EmployeeID != "")
+                            {
+                                string lastName = String.Empty;
+                                string firstName = String.Empty;
+                                string middleName = String.Empty;
+                                tb_MemberMaster TU = new tb_MemberMaster();
+                                if (SplitFullName(a.Name, out lastName, out firstName, out middleName) == "Success")
+                                {
+                                    TU.LastName = lastName;
+                                    TU.FirstName = firstName;
+                                    TU.MiddleName = middleName;
+                                    TU.MemberIDNumber = a.EmployeeID;
+                                }
+                                db.tb_MemberMaster.Add(TU);
+                                //db.SaveChanges();
+                            }
+                            else
+                            {
+                                message = String.Empty;
+                                //data.Add("<ul>");
+                                if (a.Name == "" || a.Name == null)
+                                    //data.Add("<li> name is required</li>");
+                                    message = "name is required; ";
+                                if (a.EmployeeID == "" || a.EmployeeID == null)
+                                    //data.Add("<li>ContactNo is required</li>");
+                                    message = "ContactNo is required; ";
+
+                                //data.Add("</ul>");
+                                //data.ToArray();
+                                //return Json(data, JsonRequestBehavior.AllowGet);
+                                return RedirectToAction("AdminTasks", new { xlsSelectResult = message });
+
+                            }
+                        }
+
+                        catch (DbEntityValidationException ex)
+                        {
+                            foreach (var entityValidationErrors in ex.EntityValidationErrors)
+                            {
+
+                                foreach (var validationError in entityValidationErrors.ValidationErrors)
+                                {
+
+                                    Response.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+
+                                }
+
+                            }
+                        }
+                    }
+                    //deleting excel file from folder  
+                    if ((System.IO.File.Exists(pathToExcelFile)))
+                    {
+                        System.IO.File.Delete(pathToExcelFile);
+                    }
+                    //return Json("success", JsonRequestBehavior.AllowGet);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ////alert message for invalid file format  
+                    //data.Add("<ul>");
+                    //data.Add("<li>Only Excel file format is allowed</li>");
+                    //data.Add("</ul>");
+                    //data.ToArray();
+                    message = "Only Excel file format is allowed";
+                    //return Json(data, JsonRequestBehavior.AllowGet);
+                    return RedirectToAction("AdminTasks", new { xlsSelectResult = message });
+                }
+            }
+            else
+            {
+                //if (FileUpload == null) data.Add("Please choose Excel file");
+                //data.ToArray();
+                //return Json(data, JsonRequestBehavior.AllowGet);
+                if (FileUpload == null) message = "Please choose Excel file";
+                return RedirectToAction("AdminTasks", new { xlsSelectResult = message });
+            }
         }
 
         //
